527ff34a6e234e3889a839aa645fef44b46d2c24
Ankit Aggarwal <ankit.spd@gmail.com>
2015-11-10 19:01:22 +0530

    Initial project setup

---
+++ SwiftMQTT/SwiftMQTT/SwiftMQTT.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMQTT.h
+//  SwiftMQTT
+//
+//  Created by Ankit Aggarwal on 10/11/15.
+//  Copyright © 2015 Ankit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMQTT.
+FOUNDATION_EXPORT double SwiftMQTTVersionNumber;
+
+//! Project version string for SwiftMQTT.
+FOUNDATION_EXPORT const unsigned char SwiftMQTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMQTT/PublicHeader.h>
+
+
