fb54cf00b0f2affb95c089031e66ef366b6de630
David Giovannini <dave@David.local>
2017-05-22 17:06:38 -0500

    fix dated snapshot

--- SwiftMQTT/SwiftMQTT/MQTTDatedSnapshot.swift
+++ SwiftMQTT/SwiftMQTT/MQTTDatedSnapshot.swift
@@ -8,43 +8,69 @@
 
 import Foundation
 
+private final class ReadWriteMutex {
+	private var imp = pthread_rwlock_t()
+
+	public init() {
+		pthread_rwlock_init(&imp, nil)
+	}
+	
+	deinit {
+		pthread_rwlock_destroy(&imp)
+	}
+	
+	@discardableResult public func reading<T>(_ closure: () throws -> T) rethrows -> T {
+		defer { pthread_rwlock_unlock(&imp) }
+		pthread_rwlock_rdlock(&imp)
+		return try closure()
+	}
+	
+	@discardableResult public func writing<T>(_ closure: () throws -> T) rethrows-> T {
+		defer { pthread_rwlock_unlock(&imp) }
+		pthread_rwlock_wrlock(&imp)
+		return try closure()
+	}
+}
+
 public class MQTTDatedSnapshot {
 	private let issueQueue: DispatchQueue
+    private let keepAliveTimer: DispatchSourceTimer!
 	private let interval: TimeInterval
 	private let dispatch: ([String: MQTTMessage])->()
-	
-	private var begin: clock_t? = nil
+	private let lock = ReadWriteMutex()
+
 	private var messages : [String: MQTTMessage] = [:]
 	
 	public init(label: String, interval: TimeInterval = 3.0, dispatch: @escaping ([String: MQTTMessage])->()) {
 		self.issueQueue = DispatchQueue(label: label, qos: .background, target: nil)
 		self.interval = interval
 		self.dispatch = dispatch
+		
+		keepAliveTimer = DispatchSource.makeTimerSource(queue: DispatchQueue.global())
+		keepAliveTimer.scheduleRepeating(deadline: .now() + interval, interval: interval)
+		keepAliveTimer.setEventHandler { [weak self] in
+			self?.sendNow()
+		}
+		keepAliveTimer.resume()
 	}
     
     public func sendNow() {
-        begin = nil
-        let copy = messages
-        messages.removeAll(keepingCapacity: true)
+		let local: [String: MQTTMessage] = lock.writing {
+			let copy = messages
+			messages.removeAll(keepingCapacity: true)
+			return copy
+		}
         self.issueQueue.async { [weak self] in
-            self?.dispatch(copy)
+            self?.dispatch(local)
         }
     }
 	
 	public func on(message: MQTTMessage) {
-		messages[message.topic] = message
-        if begin == nil {
-            begin = clock()
-        }
-		let now = clock()
-		let diff = TimeInterval(now - begin!) / TimeInterval(CLOCKS_PER_SEC) * 10.0
-		if diff > interval {
-			begin = now
-			let copy = messages
-			messages.removeAll(keepingCapacity: true)
-			self.issueQueue.async { [weak self] in
-				self?.dispatch(copy)
-			}
+		if messages[message.topic] != nil && message.retain {
+			return
+		}
+		lock.writing {
+			messages[message.topic] = message
 		}
 	}
 }
