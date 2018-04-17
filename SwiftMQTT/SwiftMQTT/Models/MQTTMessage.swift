375984c983f6cc84c1a3049e9201a4462642666b
Adolfo Martinelli <adolfo@airmap.com>
2018-04-17 15:27:10 -0700

    Keep payload as Data to minimize changes to public interface

--- SwiftMQTT/SwiftMQTT/Models/MQTTMessage.swift
+++ SwiftMQTT/SwiftMQTT/Models/MQTTMessage.swift
@@ -8,42 +8,20 @@
 
 import Foundation
 
-public enum MQTTPayload: CustomStringConvertible {
-    case data(Data)
-    //case file(FileHandle)
-    
-    public var stringRep: String? {
-        if case .data(let data) = self {
-            return String(data: data, encoding: .utf8)
-        }
-        return nil
-    }
-    
-    public var description: String {
-        if let str = stringRep {
-            return "'\(str)'"
-        }
-        if case .data(let data) = self {
-            return "#\(data.count)"
-        }
-        return "##"
-    }
-}
-
-public struct MQTTMessage: CustomStringConvertible {
+public struct MQTTMessage {
 	public let topic: String
-	public let payload: MQTTPayload
+	public let payload: Data
 	public let id: UInt16
 	public let retain: Bool
-	
+
 	internal init(publishPacket: MQTTPublishPacket) {
 		self.topic = publishPacket.message.topic
-		self.payload = .data(publishPacket.message.payload)
+		self.payload = publishPacket.message.payload
 		self.id = publishPacket.messageID
 		self.retain = publishPacket.message.retain
 	}
-    
-    public var description: String {
-        return "\(id)) \(topic)\(retain ? "*" : "") = \(payload)"
-    }
+
+	public var stringRepresentation: String? {
+		return String(data: self.payload, encoding: .utf8)
+	}
 }
