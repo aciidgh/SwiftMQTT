92470fc3d47e30620cb20e6b5186b50888352655
xiaopeng guan <xiaopeng.guan@ff.com>
2022-03-22 14:35:15 +0800

    Check network data length with MQTTPacketFactory

--- SwiftMQTT/SwiftMQTT/Models/MQTTSubAckPacket.swift
+++ SwiftMQTT/SwiftMQTT/Models/MQTTSubAckPacket.swift
@@ -13,10 +13,14 @@ class MQTTSubAckPacket: MQTTPacket {
     let messageID: UInt16
     let result: UInt8
     
-    init(header: MQTTPacketFixedHeader, networkData: Data) {
-        messageID = (UInt16(networkData[0]) * UInt16(256)) + UInt16(networkData[1])
-        result = networkData[2]
-        super.init(header: header)
+    init?(header: MQTTPacketFixedHeader, networkData: Data) {
+        if networkData.count >= 3 {
+            messageID = (UInt16(networkData[0]) * UInt16(256)) + UInt16(networkData[1])
+            result = networkData[2]
+            super.init(header: header)
+        } else {
+            return nil
+        }
     }
     
     var success: Bool {
